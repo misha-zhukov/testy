e2c33a701bb5bf7a3a4fb8f197846a70b5ecb684
Misha Zhukov <3v1l91l@gmail.com>
2019-11-19 13:28:01 +0200

    Add question component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbCardModule, NbThemeModule, NbLayoutModule, NbSearchModule, NbActionsModule, NbButtonModule } from '@nebular/theme';
+import { NbRadioModule, NbCardModule, NbThemeModule, NbLayoutModule, NbSearchModule, NbActionsModule,
+  NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { QuestionComponent } from './components/question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CoursesListComponent
+    CoursesListComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
@@ -28,7 +32,8 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
     NbActionsModule,
     NbButtonModule,
     HttpClientModule,
-    NbCardModule
+    NbCardModule,
+    NbRadioModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+  options = [
+    { value: 'This is value 1', label: 'Option 1' },
+    { value: 'This is value 2', label: 'Option 2' },
+    { value: 'This is value 3', label: 'Option 3' },
+    { value: 'This is value 4', label: 'Option 4' },
+  ];
+  option;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
