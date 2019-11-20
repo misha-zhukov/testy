b548176e4218cd26325294652870e0da520470db
Misha Zhukov <3v1l91l@gmail.com>
2019-11-20 15:40:39 +0200

    Copy paste code to make login and register work, refactor later

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { QuestionStepperComponent } from './components/question-stepper/question-stepper.component';
-import { NgxLoginComponent } from './@auth/components/login/login.component';
-import { NgxRegisterComponent } from './@auth/components/register/register.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'quiz/:id', component: QuestionStepperComponent },
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
-  { path: 'auth/sign-up', component: NgxLoginComponent, pathMatch: 'full'},
-  { path: 'auth/register', component: NgxRegisterComponent, pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./@auth/auth.module')
+      .then(m => m.AuthModule),
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +14,20 @@ import { CoursesListComponent } from './components/courses-list/courses-list.com
 import { QuestionComponent } from './components/question/question.component';
 import { QuestionStepperComponent } from './components/question-stepper/question-stepper.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { ThemeModule } from './@theme/theme.module';
 import { AuthModule } from './@auth/auth.module';
+import { InitUserService } from './@theme/services/init-user.service';
+import { CoreModule } from './@core/core.module';
+
+export function init_app(injector: Injector) {
+  return () =>
+    new Promise<any>((resolve: Function) => {
+      const initUserService = injector.get(InitUserService);
+      initUserService.initCurrentUser().subscribe(() => { },
+        () => resolve(), () => resolve()); // a place for logging error
+    });
+}
+
 
 @NgModule({
   declarations: [
@@ -41,8 +54,17 @@ import { AuthModule } from './@auth/auth.module';
     NbRadioModule,
     NbStepperModule,
     AuthModule.forRoot(),
+    ThemeModule.forRoot(),
+    CoreModule.forRoot(),
   ],
-  providers: [CoursesService],
+  providers: [CoursesService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: init_app,
+      deps: [Injector],
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
