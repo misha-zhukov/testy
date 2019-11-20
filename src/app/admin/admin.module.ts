3a178ce3f94df93f8581168543da57f2035dd90a
Misha Zhukov <3v1l91l@gmail.com>
2019-11-20 18:07:14 +0200

    Refactor

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,12 +4,19 @@ import { AdminComponent } from './components/admin.component';
 import { AddCourseComponent } from './components/add-course/add-course.component';
 import { AdminOperationsComponent } from './components/admin-operations/admin-operations.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { HeaderComponent } from '../components/header/header.component';
+
+import {
+  NbLayoutModule
+} from '@nebular/theme';
 
 @NgModule({
   declarations: [AdminComponent, AddCourseComponent, AdminOperationsComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NbLayoutModule,
+    HeaderComponent
   ]
 })
 export class AdminModule { }
