d7176b370c4ad236030a867de5427b89dcc93903
Misha Zhukov <3v1l91l@gmail.com>
2019-11-20 08:55:30 +0200

    Add routes to sign-up and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { QuestionStepperComponent } from './components/question-stepper/question-stepper.component';
+import { NgxLoginComponent } from './@auth/components/login/login.component';
+import { NgxRegisterComponent } from './@auth/components/register/register.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'quiz/:id', component: QuestionStepperComponent },
-  { path: '', redirectTo: '/landing', pathMatch: 'full' }
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'auth/sign-up', component: NgxLoginComponent, pathMatch: 'full'},
+  { path: 'auth/register', component: NgxRegisterComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
